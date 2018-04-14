ef6bd74a9bd19ee3338411027ac7f71628b36549
frostblooded <frostblooded@yahoo.com>
2018-04-14 17:28:44 +0300

    Remove some old stuff

--- test/controllers/image_controller_test.rb
+++ test/controllers/image_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class ImageControllerTest < ActionDispatch::IntegrationTest
-  test "should get convert" do
-    get image_convert_url
-    assert_response :success
-  end
-
 end
