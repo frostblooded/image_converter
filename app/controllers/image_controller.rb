ca572ae03b456a00f23dd49608f22cf83d1e549a
frostblooded <frostblooded@yahoo.com>
2018-04-14 17:20:14 +0300

    Make image conversion on submit

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,5 +1,21 @@
 class ImageController < ApplicationController
   def convert
-    params['original_image']
+    image_param = params[:original_image]
+    image_file = image_param.tempfile
+
+    original_format = image_param.content_type.split('/').last
+    desired_format = params[:format]
+
+    file_id = image_file.object_id.to_s # Use the object id to name the file
+    original_name = file_id + '.' + original_format
+    desired_name = file_id + '.' + desired_format
+
+    File.open(original_name, 'wb+') do |f|
+      f.write image_file.read
+    end
+
+    `convert #{original_name} #{desired_name}`
+
+    redirect_to root_url
   end
 end
