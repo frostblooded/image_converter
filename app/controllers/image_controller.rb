89ef94b9241b3aaee6a4a6a3887c7b029c7af66f
frostblooded <frostblooded@yahoo.com>
2018-04-15 15:44:15 +0300

    Refactor the code a bit

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,10 +1,9 @@
 class ImageController < ApplicationController
-  IMAGE_TMP_SAVE_PATH = 'tmp/'
-  BUCKET_PUBLIC_URL = 'https://s3.eu-west-3.amazonaws.com/bg.image.converter/'
-  BUCKET_NAME = 'bg.image.converter'
+  IMAGE_TMP_SAVE_PATH = 'tmp/'.freeze
+  BUCKET_PUBLIC_URL = 'https://s3.eu-west-3.amazonaws.com/bg.image.converter/'.freeze
 
   def convert
-    # TODO: I should add checking for the parameters. Make sure invalid formats aren't being passed in.
+    # TODO: Make sure invalid formats aren't being passed in.
     image_file = params[:original_image].tempfile
 
     # TODO: Check if original format equals desired format
@@ -18,20 +17,10 @@ class ImageController < ApplicationController
     file_id = get_file_id image_file
     original_name = get_original_name file_id, original_format
     desired_name = get_desired_name file_id, desired_format
+    ImageHelper.upload_converted_file image_file, original_name,
+                                      desired_name, file_id
 
-    # TODO: Save the files in the tmp directory while working with them
-    ImageHelper.save_file_to_path image_file, original_name
-
-    # TODO: Run the conversion in a Docker container
-    # TODO: The status code of the operation should be handled. For example if there was an error.
-    `convert #{original_name} #{desired_name}`
-
-    ImageHelper.upload_file_to_s3 image_file, file_id, BUCKET_NAME
-    File.delete original_name
-    File.delete desired_name
-
-    # TODO: Redirect to a new page that shows the new image and lets you download it or convert another image
-    redirect_to image_url(image_object.key)
+    redirect_to image_url(file_id)
   end
 
   def show
@@ -39,24 +28,25 @@ class ImageController < ApplicationController
     @image_url = BUCKET_PUBLIC_URL + image_id
   end
 
-private
+  private
+
   def get_original_format(params)
     params[:original_image].content_type.split('/').last
   end
 
-  def get_original_name file_id, original_format
+  def get_original_name(file_id, original_format)
     get_file_name file_id, original_format
   end
 
-  def get_desired_name  file_id, desired_format
+  def get_desired_name(file_id, desired_format)
     get_file_name file_id, desired_format
   end
 
-  def get_file_name base_file_name, format
+  def get_file_name(base_file_name, format)
     IMAGE_TMP_SAVE_PATH + base_file_name + '.' + format
   end
 
-  def get_file_id file
+  def get_file_id(file)
     file.object_id.to_s
   end
 end
--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,13 +1,35 @@
 module ImageHelper
+  BUCKET_NAME = 'bg.image.converter'.freeze
+
   def self.save_file_to_path(file, path)
     File.open(path, 'wb+') do |f|
       f.write file.read
     end
   end
 
-  def self.upload_file_to_s3(file, key, bucket_name)
-    s3 = Aws::S3::Resource.new
-    bucket = s3.bucket(bucket_name)
-    image_object = bucket.put_object({body: file, key: file_id})
+  def self.upload_file_to_s3(file_path, desired_name, bucket_name)
+    # s3 = Aws::S3::Resource.new
+    # bucket = s3.bucket(bucket_name)
+    # image_object = bucket.put_object({body: file, key: file_id})
+
+    # Of course it would probably be best to use the AWS gem here, but for
+    # some reason it takes about 30 seconds to upload an image
+    `~/.local/bin/aws s3 cp #{file_path} s3://#{bucket_name}/#{desired_name}`
+  end
+
+  def self.convert_file(file, original_name, desired_name)
+    ImageHelper.save_file_to_path file, original_name
+
+    # TODO: Run the conversion in a Docker container
+    # TODO: The status code of the operation should be handled. For example if there was an error.
+    `convert #{original_name} #{desired_name}`
+
+    File.delete original_name
+  end
+
+  def self.upload_converted_file(file, original_name, desired_name, file_id)
+    ImageHelper.convert_file file, original_name, desired_name
+    ImageHelper.upload_file_to_s3 desired_name, file_id, BUCKET_NAME
+    File.delete desired_name
   end
 end
