d27147362cc51b720684ee6b536ed33503e21980
frostblooded <frostblooded@yahoo.com>
2018-04-26 17:02:13 +0300

    Write tests for file saving and file uploading

--- spec/helpers/image_helper_spec.rb
+++ spec/helpers/image_helper_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 require 'open-uri'
 
-describe(ImageHelper) do
-  before :each do
+describe ImageHelper do
+  IMAGE_PATH = File.expand_path(File.join(__dir__, '..', 'image')).freeze
+
+  before :all do
     FakeFS.activate!
+  end
+
+  before do
+    @original_name = IMAGE_PATH + '.png'
+    FakeFS::FileSystem.clone(@original_name)
+
+    # Tempfile uses /tmp, but it isn't in FakeFS
+    Dir.mkdir '/tmp' unless Dir.exist? '/tmp'
+
+    @tempfile = Tempfile.open
+
+    File.open @original_name, 'r' do |f|
+      @tempfile.write(f.read)
+    end
   end
 
   describe '#save_file_to_path' do
@@ -18,23 +34,6 @@ describe(ImageHelper) do
   end
 
   describe '#convert_file' do
-    IMAGE_PATH = File.expand_path(File.join(__dir__, '..', 'image')).freeze
-
-    before do
-      @original_name = IMAGE_PATH + '.png'
-      FakeFS::FileSystem.clone(@original_name)
-
-      # Setup tempfile
-      # Tempfile uses /tmp, but it isn't in FakeFS
-      Dir.mkdir '/tmp' unless Dir.exist? '/tmp'
-
-      @tempfile = Tempfile.open
-
-      File.open @original_name, 'r' do |f|
-        @tempfile.write(f.read)
-      end
-    end
-
     it 'doesn\'t do anything if original and desired name are equal' do
       @desired_name = @original_name
       expect(ImageHelper).not_to receive(:convert_in_container)
@@ -47,14 +46,29 @@ describe(ImageHelper) do
       ImageHelper.convert_file @tempfile, @original_name, @desired_name
       expect(Dir.glob(@desired_name).length).to eq 1
     end
+  end
 
-    after do
-      @tempfile.close
-      @tempfile.unlink
+  describe '#upload_converted_file' do
+    it 'removes file after upload' do
+      allow(ImageHelper).to receive :upload_file_to_s3
+      allow(ImageHelper).to receive :test_lambda_call
+      allow(ImageHelper).to receive :test_ses
+
+      @desired_name = IMAGE_PATH + '.jpg'
+
+      ImageHelper.upload_converted_file @tempfile, @original_name,
+                                        @desired_name, SecureRandom.hex
+
+      expect(Dir.glob(@desired_name).length).to eq 0
     end
   end
 
-  after :each do
+  after do
+    @tempfile.close
+    @tempfile.unlink
+  end
+
+  after :all do
     FakeFS.deactivate!
   end
 end
