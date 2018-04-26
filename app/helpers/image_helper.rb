d948994b15b1a1ac325e7713b9713a7a03fc212b
frostblooded <frostblooded@yahoo.com>
2018-04-26 16:36:40 +0300

    Write two tests for conversion

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -9,10 +9,6 @@ module ImageHelper
   end
 
   def self.upload_file_to_s3(file_path, desired_name, bucket_name)
-    # s3 = Aws::S3::Resource.new
-    # bucket = s3.bucket(bucket_name)
-    # image_object = bucket.put_object({body: file, key: file_id})
-
     # Of course it would probably be best to use the AWS gem here, but for
     # some reason it takes about 30 seconds to upload an image when I am using it,
     # but it works fine like this
@@ -23,13 +19,16 @@ module ImageHelper
     # Don't actually convert if input and output are the same
     return if original_name == desired_name
 
-    # TODO: Use docker compose to run the conversion container with the server
     # TODO: Handle conversion status code
-    # TODO: Containerize the server and make it start the conversion container
     # Presumes that the Docker container is already running in the background
-    # Find docker based on its image (AKA ancestor)
+    # Finds docker container based on its image (AKA ancestor)
     container_filters = { ancestor: [DOCKER_CONVERSION_IMAGE] }.to_json
     container = Docker::Container.all(filters: container_filters).first
+    ImageHelper.convert_in_container file, original_name,
+                                     desired_name, container
+  end
+
+  def self.convert_in_container(file, original_name, desired_name, container)
     container.store_file(original_name, file.read)
     container.exec(['convert', original_name, desired_name])
     save_file_to_path(container.read_file(original_name), desired_name)
--- spec/helpers/image_helper_spec.rb
+++ spec/helpers/image_helper_spec.rb
@@ -1,14 +1,61 @@
 require 'rails_helper'
+require 'open-uri'
 
-describe '#save_file_to_path' do
-  it 'creates file' do
-    file_name = '3.txt'
-    file = Tempfile.new file_name
+describe(ImageHelper) do
+  before :each do
+    FakeFS.activate!
+  end
 
-    FakeFS do
-      ImageHelper.save_file_to_path file, file_name
-      file_exists = Dir.glob(file_name).length >= 1
-      expect(file_exists).to be_truthy
+  describe '#save_file_to_path' do
+    it 'creates file with correct contents' do
+      file_name = '3.txt'
+      text = SecureRandom.hex
+
+      ImageHelper.save_file_to_path text, file_name
+      expect(Dir.glob(file_name).length).to eq 1
+      expect(File.read(file_name)).to eq text
     end
   end
+
+  describe '#convert_file' do
+    IMAGE_PATH = File.expand_path(File.join(__dir__, '..', 'image')).freeze
+
+    before do
+      @original_name = IMAGE_PATH + '.png'
+      FakeFS::FileSystem.clone(@original_name)
+
+      # Setup tempfile
+      # Tempfile uses /tmp, but it isn't in FakeFS
+      Dir.mkdir '/tmp' unless Dir.exist? '/tmp'
+
+      @tempfile = Tempfile.open
+
+      File.open @original_name, 'r' do |f|
+        @tempfile.write(f.read)
+      end
+    end
+
+    it 'doesn\'t do anything if original and desired name are equal' do
+      @desired_name = @original_name
+      expect(ImageHelper).not_to receive(:convert_in_container)
+      ImageHelper.convert_file @tempfile, @original_name, @desired_name
+      expect(Dir.glob(@desired_name).length).to eq 1
+    end
+
+    it 'converts and deletes files correctly' do
+      @desired_name = IMAGE_PATH + '.jpg'
+      ImageHelper.convert_file @tempfile, @original_name, @desired_name
+      expect(Dir.glob(@desired_name).length).to eq 1
+    end
+
+    after do
+      @tempfile.close
+      @tempfile.unlink
+    end
+  end
+
+  after :each do
+    FakeFS.deactivate!
+  end
 end
+
